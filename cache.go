ecbb385d8e8b53753ad020a15ebc1e4f9c58e1f6
Ernesto Oltra <ernestokarim@gmail.com>
2012-07-01 08:36:54 +0200

    Added functions to save and load the cache.

--- cache.go
+++ cache.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/gob"
+	"fmt"
+	"log"
 	"os"
+	"path"
 	"time"
 )
 
@@ -24,4 +28,44 @@ func CacheModified(filename string) (bool, error) {
 	}
 
 	return false, nil
+}
+
+// Load the caches from a file.
+func LoadCache() error {
+	name := path.Join(conf.Build, "cache")
+
+	// Open the cache file if it exists
+	f, err := os.Open(name)
+	if err != nil && os.IsNotExist(err) {
+		return nil
+	} else if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	log.Println("Reading deps cache:", name)
+
+	// Decode the caches
+	if err := gob.NewDecoder(f).Decode(&modificationCache); err != nil {
+		return fmt.Errorf("cannot decode the deps cache: %s", err)
+	}
+
+	return nil
+}
+
+// Save the caches to a file.
+func WriteCache() error {
+	// Create the cache file
+	f, err := os.Create(path.Join(conf.Build, "cache"))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Encode the caches
+	if err := gob.NewEncoder(f).Encode(&modificationCache); err != nil {
+		return fmt.Errorf("cannot encode the deps cache: %s", err)
+	}
+
+	return nil
 }
